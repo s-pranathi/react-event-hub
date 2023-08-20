21ff6c95e107f5ce6a321fcc65697d8f2614e9ef
Annie Yao <annieyao.mango@gmail.com>
2023-08-19 20:27:09 -0700

    adds Header and Footer component, bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
-function App() {
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
   return (
     <div>
+      <Header/>
       <h1>eventHub</h1>
+      <Footer/>
     </div>
   );
 }
