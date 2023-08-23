bc49d894c28eda485deaa6c08f44578bacf27cc6
Annie Yao <annieyao.mango@gmail.com>
2023-08-23 15:07:19 -0700

    changing Nav.Link cart text

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,7 +10,7 @@
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className='ms-auto'>
-              <Nav.Link href="/cart"><i className='fas fa-solid fa-calendar-plus'></i>Add Event</Nav.Link>
+              <Nav.Link href="/cart"><i className='fas fa-solid fa-calendar-plus'></i>My Event Cart</Nav.Link>
               <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
             </Nav>
           </Navbar.Collapse>
