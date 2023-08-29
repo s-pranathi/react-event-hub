510897993f8371b6dac35bd5ef17a654f7616630
ndr911 <44654040+ndr911@users.noreply.github.com>
2023-08-28 22:32:11 -0400

    update images

---
+++ frontend/src/events.js
@@ -0,0 +1,101 @@
+const events = [
+    {
+      _id: '1',
+      name: 'Maggie Rogers Live',
+      image: '/images/maggie.jpeg',
+      description:
+      'Opportunity to see Maggie Roger Live ',
+      category: 'Concert',
+      price: 75,
+      venue: 'Dolby Theater',
+      countInStock: 200,
+      participants: 100,
+    },
+    {
+        _id: '2',
+        name: 'BBQ Battle',
+        image: '/images/bbq.jpeg',
+        description:
+        'Top bbq restaurants will be competing in a bbq competition. Come taste the best bbq',
+        category: 'Food & Drink',
+        price: 125,
+        venue: 'Grace Park',
+        countInStock: 250,
+        participants: 200,
+    },
+    {
+        _id: '3',
+        name: 'Bingo Night',
+        image: '/images/bingo.png',
+        description:
+        'Enjoy a chill night playing bingo with friends and family. Lots of prizes for winners',
+        category: 'Game Night',
+        price: 15,
+        venue: 'Hugh Community Center',
+        countInStock: 100,
+        participants: 2,
+      
+  },
+  {
+    _id: '4',
+    name: 'Volleyball Tournamet',
+    image: '/images/volley.jpeg',
+    description:
+    'Come Enjoy a night of fun friendly volleball. Bring a group of 8 to be eligable to enter!',
+    category: 'Sports',
+    price: 0,
+    venue: 'Rock Creek Park',
+    countInStock: 50,
+    participants: 15,
+  },
+  {
+    _id: '5',
+    name: 'Womens National Team Soccer Match vs Spain',
+    image: '/images/soccerball.jpeg',
+    description:
+    'Watch the US womens national team take on Spain',
+    category: 'Sports',
+    price: 275,
+    venue: 'Audi Stadium',
+    countInStock: 400,
+    participants: 175,
+  },
+  {
+    _id: '6',
+    name: 'Coding Bootcamp',
+    image: '/images/coding.jpeg',
+    description:
+    'Coding Bootcamp intro into React. Learn the basics and explore what can be done with this tech',
+    category: 'Education',
+    price: 25,
+    venue: 'Grant Hall-UMD',
+    countInStock: 40,
+    participants: 30,
+  },
+  {
+    _id: '7',
+    name: 'Marvel Screening - Ant Man',
+    image: '/images/marvel.png',
+    description:
+    'Live outdoor screen of the Ant Man',
+    category: 'Entertainment',
+    price: 13,
+    venue: 'Mosaic part',
+    countInStock: 75,
+    participants: 70,
+  },
+  {
+    _id: '8',
+    name: 'Board Game Night',
+    image: '/images/boardgames.jpeg',
+    description:
+    'Come out in the community and enjoy a night of fun board game competitions',
+    category: 'Game Night',
+    price: 0,
+    venue: 'Lola Community Center',
+    countInStock: 30,
+    participants: 15,
+  },
+]
+
+export default events
