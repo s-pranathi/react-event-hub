bf5e76df9cbef3bf1e4b7b0bde72fbb18ab9fde6
ndr911 <44654040+ndr911@users.noreply.github.com>
2023-08-28 22:31:56 -0400

    added components and event json

---
+++ frontend/src/components/Event.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Participant from './Participant'
+import { Link } from 'react-router-dom'
+
+const Event = ({event}) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <Link to={`/event/${event._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
+      <Card.Body>
+        <Link to={`/event/${event._id}`}>
+          <Card.Title as='div'>
+            <strong>{event.name}</strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as='div'>
+          <Participant value={event.participant} />
+        </Card.Text>
+
+        <Card.Text as='h3'>
+          ${event.price}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Event
