42fbf3a29cf6724217e4a853fd12c7ece8c5b181
ndr911 <44654040+ndr911@users.noreply.github.com>
2023-09-14 22:42:41 -0400

    updated participant image

--- frontend/src/components/Participant.js
+++ frontend/src/components/Participant.js
@@ -8,50 +8,40 @@ const Participant = ({value, text, color}) => {
         <i style={{color}}
           className={
             value >= 1
-              ? 'fas fa-star'
-              : value >= 0.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+              ? 'fa-solid fa-person'
+              : value >= 18
           }></i>
       </span>
       <span>
         <i style={{color}}
           className={
-            value >= 2
-              ? 'fas fa-star'
-              : value >= 1.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+            value >= 20
+              ? 'fa-solid fa-person'
+              : value >= 10
           }></i>
       </span>
       <span>
         <i style={{color}}
           className={
-            value >= 3
-              ? 'fas fa-star'
-              : value >= 2.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+            value >= 30
+              ? 'fa-solid fa-person'
+              : value >= 25
           }></i>
       </span>
       <span>
         <i style={{color}}
           className={
-            value >= 4
-              ? 'fas fa-star'
-              : value >= 3.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+            value >= 40
+              ? 'fa-solid fa-person'
+              : value >= 35
           }></i>
       </span>
       <span>
         <i style={{color}}
           className={
-            value >= 5
-              ? 'fas fa-star'
-              : value >= 4.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+            value >= 50
+              ? 'fa-solid fa-person'
+              : value >= 45
           }></i>
       </span> 
       <span>{text}</span>
