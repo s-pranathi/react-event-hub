6024ffa201a9d10ff159a566809b446b12e48924
s-pranathi <singaripranathi@gmail.com>
2023-09-20 17:47:28 -0500

    Install Axios, get events from backend in Home Screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import {Col, Row} from 'react-bootstrap'
-import events from '../events'
-import Event from '../components/Event'
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import axios from "axios";
+import Event from "../components/Event";
 
 const HomeScreen = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data } = await axios.get("/api/events");
+      setEvents(data);
+    };
+    fetchEvents();
+  });
+
   return (
     <>
-    <h1>Latest Events</h1>
-    <Row>
-        {events.map(e => (
-            <Col sm={12} md={6} lg={4} xl={3}>
-                <Event event={e}/>
-            </Col>
-        ))
-
-        }
-    </Row>
+      <h1>Latest Events</h1>
+      <Row>
+        {events.map((e) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Event event={e} />
+          </Col>
+        ))}
+      </Row>
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
