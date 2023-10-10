db50581bc5e97d51513b6352e673d9fe9ab7489b
s-pranathi <singaripranathi@gmail.com>
2023-10-10 18:35:17 -0500

    Add login api, user login reducer & actions

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import eventRoutes from "./routes/eventRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
 
 const app = express();
 dotenv.config();
 connectDB();
 
+app.use(express.json());
 app.use("/api/events", eventRoutes);
+app.use("/api/users", userRoutes);
+
 app.use(errorHandler);
 
 app.listen(5002, console.log("Server is running on port 5002"));
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,23 @@
-import {configureStore, combineReducers} from '@reduxjs/toolkit'
-import { eventListReducer } from './reducers/eventReducers'
-
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { eventListReducer } from "./reducers/eventReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const rootReducer = combineReducers({
-    eventList: eventListReducer
-})
+  eventList: eventListReducer,
+  userLogin: userLoginReducer,
+});
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const store = configureStore({
-    reducer: rootReducer,
-    preloadedState: {}
-})
+  reducer: rootReducer,
+  preloadedState: initialState,
+});
 
-export default store
+export default store;
