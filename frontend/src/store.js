a5556deffe8f7afccdd85651dac2c7206c3e389f
Annie Yao <annieyao.mango@gmail.com>
2023-10-09 20:50:59 -0700

    event constants, reducers, update store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,13 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
+import { eventListReducer } from './reducers/eventReducers'
+
+
+const rootReducer = combineReducers({
+    eventList: eventListReducer
+})
 
 const store = configureStore({
-    reducer: {},
+    reducer: rootReducer,
     preloadedState: {}
 })
 
