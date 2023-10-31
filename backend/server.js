19ee8d2fd72ab2a2065ddac29ec0f6895f872fde
Annie Yao <annieyao.mango@gmail.com>
2023-10-30 20:09:05 -0700

    Prepared for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from 'path';
 import connectDB from "./config/db.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -18,6 +19,14 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-app.use(errorHandler);
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
-app.listen(5002, console.log("Server is running on port 5002"));
+app.use(errorHandler);
+const port = process.env.PORT
+app.listen(port, console.log(`Server is running on port ${port}.`));
